49eeaa9b72aab6ffcba151b8edc1e92368528f4f
Eliseo Alcaraz <elipalcaraz@gmail.com>
2025-05-14 02:14:58 +0800

    add dynamic title card for hosting

--- app/(root)/hosting/page.tsx
+++ app/(root)/hosting/page.tsx
@@ -1,12 +1,27 @@
+import CourseCard from '@/components/CourseCard'
 import React from 'react'
+
+const courses = [
+  { id: 1, title: "Company" },
+  { id: 2, title: "Sport Org" },
+  { id: 3, title: "Group Project" },
+  { id: 4, title: "Company 2" },
+];
 
 const Hosting = () => {
   return (
-    <div className='min-h-screen flex flex-col items-center justify-start w-full gap-10 pt-10'>
-      <div>
-        <div className='flex  items-center  justify-center bg-myred text-white font-medium text-lg px-20  py-5 rounded-full cursor-pointer shadow-[0px_4px_4px_rgba(0,0,0,0.25)] backdrop-blur-[4px]'>
-          <img src="/create.svg" alt="Create" className="w-8 h-8" />
-          <p className='underline'>Hosting</p>
+    <div className='w-full min-h-screen flex justify-center'>
+      <div className='min-h-screen flex flex-col items-center justify-start w-full max-w-md gap-10 pt-10'>
+        <div className='w-full p-8'>
+          <div className='flex  items-center justify-center bg-myred text-white font-medium text-lg px-20 py-5 rounded-full cursor-pointer shadow-[0px_4px_4px_rgba(0,0,0,0.25)] backdrop-blur-[4px]'>
+            <img src="/create.svg" alt="Create" className="w-8 h-8" />
+            <p className='underline'>Hosting</p>
+          </div>
+        </div>
+        <div className="flex flex-col w-full px-8 gap-6">
+          {courses.map((course) => (
+            <CourseCard key={course.id} title={course.title} />
+          ))}
         </div>
       </div>
     </div>
--- components/CourseCard.tsx
+++ components/CourseCard.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+"use client"
 
-const CourseCard = () => {
+import { Button } from "./ui/button"
+
+
+type CourseCardProps = {
+    title: string;
+    onClick?: () => void;
+}
+
+
+const CourseCard = ({ title, onClick}: CourseCardProps) => {
   return (
-    <div>CourseCard</div>
+    <div className="w-full">
+        <Button onClick={onClick} className="w-full h-24 bg-myred text-white font-medium text-lg rounded-2xl shadow-md backdrop-blur-[4px]">
+            {title}
+        </Button>
+    </div>
   )
 }
 
-export default CourseCard
+export default CourseCard;
