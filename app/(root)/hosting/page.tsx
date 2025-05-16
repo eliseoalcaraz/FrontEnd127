e614ac0f6ffc52037a61072dc8a63afbaeac0bab
Eliseo Alcaraz <elipalcaraz@gmail.com>
2025-05-16 21:14:34 +0800

    feat(hosting): add create button for course

--- app/(root)/hosting/page.tsx
+++ app/(root)/hosting/page.tsx
@@ -1,11 +1,26 @@
+"use client"
+
 import CourseCard from '@/components/CourseCard'
-import React from 'react'
-import { courses } from '@/content/data'
+import React, { useState } from 'react'
+import { courses as initialCourses } from '@/content/data'
 
 const Hosting = () => {
+
+  const [courses, setCourses] = useState(initialCourses)
+
+  const handleCreateCourse = () => {
+    //create a separate component for creating the course
+
+    const newCourse = {
+      id: courses.length + 1,
+      title: "New Course"
+    }
+    setCourses(prev => [...prev, newCourse])
+  }
+
   return (
     <div className='w-full min-h-screen flex justify-center'>
-      <div className='min-h-screen flex flex-col items-center justify-start w-full max-w-md gap-10 pt-10'>
+      <div className='min-h-screen flex flex-col items-center justify-start w-full max-w-md gap-10 pt-10 mb-10'>
         <div className='w-full p-8'>
           <div className='flex gap-2 items-center justify-center bg-myred text-white font-medium text-lg px-20 py-5 rounded-full cursor-pointer shadow-[0px_4px_4px_rgba(0,0,0,0.25)] backdrop-blur-[4px]'>
             <img src="/create.svg" alt="Create" className="w-8 h-8" />
@@ -16,6 +31,9 @@ const Hosting = () => {
           {courses.map((course) => (
             <CourseCard key={course.id} title={course.title} />
           ))}
+        </div>
+        <div className='bg-myred h-14 w-14 rounded-full cursor-pointer flex items-center justify-center' onClick={handleCreateCourse}>
+          <img src="/add.svg" alt="Create" className="w-8 h-8" />
         </div>
       </div>
     </div>
